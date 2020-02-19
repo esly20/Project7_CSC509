9aa2fbd0af600494d08fb8b92a9ac1ad07814ad6
Erik <58671211+erikfotta@users.noreply.github.com>
2020-02-19 12:45:54 -0500

    End of class 2/19 - Erik

---
+++ Project7/Project7/AnnouncementsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AnnouncementsTableViewCell.swift
+//  Project7
+//
+//  Created by Erik Fotta on 2/19/20.
+//  Copyright © 2020 CSC509. All rights reserved.
+//
+
+import UIKit
+
+class AnnouncementsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
