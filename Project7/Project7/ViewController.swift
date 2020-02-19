0e05f0956a3d16f47928dcd360c5ea9a25cfbeb5
esly20 <emslibeck@gmail.com>
2020-02-18 21:45:01 -0500

    xCode Project Created

---
+++ Project7/Project7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by student on 2/18/20.
+//  Copyright © 2020 Emma Slibeck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
