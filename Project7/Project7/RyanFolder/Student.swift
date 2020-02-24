65caef4874b52ed046183653d2840c00bfeced48
Ryan Mai <rmai21@andover.edu>
2020-02-24 10:32:09 -0500

    Made Student struct, Faculty struct, and Class struct

---
+++ Project7/Project7/RyanFolder/Student.swift
@@ -0,0 +1,24 @@
+//
+//  Student.swift
+//  Project7
+//
+//  Created by Ryan Mai on 2/24/20.
+//  Copyright © 2020 CSC509. All rights reserved.
+//
+
+import Foundation
+
+struct Student: Codable {
+    var name: String
+    var preferredName: String
+    var email: String
+    var boarder: Boolean
+    var dorm: String?
+    var cluster: String
+    var program: String
+    var prepaidCollege: Boolean
+    
+    var classOne: Class
+    var classTwo: Class
+    var afternoonActivity: Class
+}
