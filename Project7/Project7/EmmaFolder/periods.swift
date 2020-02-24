49c1380eb3bc3a0bff8223aaa2be33fdacf09552
esly20 <emslibeck@gmail.com>
2020-02-24 14:46:43 -0500

    Working on CollectionView files

---
+++ Project7/Project7/EmmaFolder/periods.swift
@@ -0,0 +1,13 @@
+//
+//  periods.swift
+//  Project7
+//
+//  Created by student on 2/24/20.
+//  Copyright © 2020 CSC509. All rights reserved.
+//
+
+import Foundation
+
+struct Periods: Codable{
+    var results: [Schedules]
+}
---
+++ Project7/Project7/EmmaFolder/schedules.swift
@@ -0,0 +1,18 @@
+//
+//  schedules.swift
+//  Project7
+//
+//  Created by student on 2/24/20.
+//  Copyright © 2020 CSC509. All rights reserved.
+//
+
+import Foundation
+
+struct Schedules: Codable {
+    var teacher: String
+    var class_title: String
+    var start_time: String
+    var end_time: String
+    var locations: String
+
+}
