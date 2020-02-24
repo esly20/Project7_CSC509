dfb9b3d82f654af3b9ba081e1bcdaab51038a7dd
lilpapertowel <liltowel@users.noreply.github.com>
2020-02-24 12:45:37 -0500

    New FIle

---
+++ Project7/Project7/InfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InfoViewController.swift
+//  Project7
+//
+//  Created by Patrick Alston on 2/21/20.
+//  Copyright © 2020 CSC509. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
