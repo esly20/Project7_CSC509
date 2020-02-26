1350152de55e0d9da953d4c9e15133799f280eb9
sseth18 <samar.seth@gmail.com>
2020-02-26 13:05:08 -0500

    populated the table view controller and set up the detail view controller for selected cells

--- Project7/Project7/SamarFolder/BuildingListTableViewController.swift
+++ Project7/Project7/SamarFolder/BuildingListTableViewController.swift
@@ -9,86 +9,62 @@
 import UIKit
 
 class BuildingListTableViewController: UITableViewController {
-    let dorms: [String] = []
-    let buildings: [String] = []
+    let defaults = UserDefaults.standard
+    
+    let dorms: [String] = ["Day Hall", "Paul Revere Hall",  "Bartlet Hall", "Foxcroft Hall", "Smith House", "Burtt House", "Newman House", "Stuart Hall", "Fuess Hall", "Nathan Hale House", "Stimson House", "Stevens House", "Adams Hall", "Taylor Hall", "Johnson Hall", "Thompson House", "Jewett-Tucker House", "Blanchard House", "Bishop Hall", "Rockwell Hall", "Isham Dormitory", "Bancroft Hall", "Pemberton Cottage", "Eaton Cottage", "Andover Cottage", "Pease House", "Williams Hall Carriage House", "Double Brick House", "Samaritan House",  "Stearns House", "Stowe House", "French House", "Whitney House", "Carter House", "Clement House", "Draper Cottage", "America House", "Alumni House", "Bertha Bailey House", "Flagg House", "Morton House", "Allen House"]
+    let buildings: [String] = ["Oliver Wendell Holmes Library", "Morse Hall", "Gelb Science Center", "Sykes Wellness Center", "Bullfinch", "Borden and Memorial Gym", "Shuman Admissions Center", "Smith Center", "Case Memorial Cage", "Snyder Center", "Ice Rink", "Samuel Phillips Hall", "George Washington Hall", "Elson Art Center", "Addison Gallery of American Art", "Cochran Chapel", "Graves Hall", "Pearson Hall", "Robert S. Peabody Institute of Archaeology"]
+    let otherPOI: [String] = ["Phelps Park", "Tennis Courts", "Sorota Track Complex", "Phelps Stadium", "Siberia", "Graves Field", "Moncrief M. Cochran Sanctuary"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 2
+        return 3
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         if section == 0 {
             return buildings.count
-        } else {
+        } else if section == 1 {
             return dorms.count
+        } else {
+            return otherPOI.count
         }
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as! TableViewCell
-
-        cell.name!.text = buildings[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        if indexPath.section == 0 {
+            cell.textLabel!.text = buildings[indexPath.row]
+        } else if indexPath.section == 1 {
+            cell.textLabel!.text = dorms[indexPath.row]
+        } else {
+            cell.textLabel!.text = otherPOI[indexPath.row]
+        }
 
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return  "Buildings"
+        } else if section == 1 {
+            return "Dorms"
+        } else {
+            return "Other"
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        defaults.set(indexPath.section, forKey: "selectedSection")
+        defaults.set(indexPath.row, forKey: "selectedCell")
+        
+        self.performSegue(withIdentifier: "selectedCell", sender: self)
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
