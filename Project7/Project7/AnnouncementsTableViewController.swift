406e9d4757124524e9886024c080616bb894c383
Erik <58671211+erikfotta@users.noreply.github.com>
2020-02-26 00:53:13 -0500

    announcements work

--- Project7/Project7/AnnouncementsTableViewController.swift
+++ Project7/Project7/AnnouncementsTableViewController.swift
@@ -21,6 +21,10 @@ class AnnouncementsTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("yay")
+    }
 
     // MARK: - Table view data source
 
