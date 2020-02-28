eafacfbe14c1cbc3e93d213dce93f5c2ed321731
mwitt20 <mwitt20@andover.edu>
2020-02-28 14:02:59 -0500

    adding notification request and some settings changes

--- Project7/Project7/AppDelegate.swift
+++ Project7/Project7/AppDelegate.swift
@@ -7,14 +7,16 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.badge,.sound]) { (granted, error) in
+        }
         
         return true
     }
