f6f51fa67da53a8e3b6496db00c1a44ff88bb11d
Ryan Mai <rmai21@andover.edu>
2020-03-01 14:53:19 -0500

    Updated Profile VC

--- Project7/Project7/RyanFolder/ProfileViewController.swift
+++ Project7/Project7/RyanFolder/ProfileViewController.swift
@@ -12,12 +12,28 @@ class ProfileViewController: UIViewController {
     
     var data: DataLayer = DataLayer(userID: 1)
     
+    //Outlets
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var programLabel: UILabel!
+    @IBOutlet weak var clusterDormLabel: UILabel!
+    
+    
     override func viewDidAppear(_ animated: Bool) {
         viewDidLoad()
         
-        let _: Student = data.getStudentInfo()!
-        let _: Team = data.getStudentTeam()!
+        let mainStudent: Student = data.getStudentInfo()!
+
+        print(mainStudent)
         
+        fullNameLabel.textColor = UIColor(red: 102/255, green: 173/255, blue: 220/255, alpha: 1)
+        
+        fullNameLabel.text =  "\(mainStudent.firstname) \(mainStudent.lastname)"
+        programLabel.text = mainStudent.program
+        if mainStudent.boarder == true {
+            clusterDormLabel.text = "\(mainStudent.cluster) • \(mainStudent.dorm)"
+        } else {
+            clusterDormLabel.text = "\(mainStudent.cluster) • Day Student"
+        }
     }
 
     override func viewDidLoad() {
