b0098e89b45efebab44a15786e93c635f6ea8344
esly20 <emslibeck@gmail.com>
2020-03-01 16:47:47 -0500

    My activities update

---
+++ Project7/Project7/EmmaFolder/studentActivity.swift
@@ -0,0 +1,17 @@
+//
+//  studentActivity.swift
+//  Project7
+//
+//  Created by student on 3/1/20.
+//  Copyright © 2020 CSC509. All rights reserved.
+//
+
+import Foundation
+struct StudentActivity: Codable{
+        var activityid: Int
+        var name: String
+        var time_start: String
+        var time_end: String
+        var description: String
+        var location: String
+    }
