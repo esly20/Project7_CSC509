62d0a800a1d68c8a70c206e1ac187333dc74268d
esly20 <emslibeck@gmail.com>
2020-03-02 14:45:08 -0500

    sign up updates

--- Project7/Project7/EmmaFolder/ActivityTableViewController.swift
+++ Project7/Project7/EmmaFolder/ActivityTableViewController.swift
@@ -140,6 +140,7 @@ class ActivityTableViewController: UITableViewController {
        defaults.set(slcActivity, forKey: "slcActivity" )
        navigationController?.pushViewController(selectedVC, animated: true)
    }
+
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
