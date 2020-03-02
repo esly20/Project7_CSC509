6b432e7fa997bb4b4e84de5c7dc082dba5756d48
Erik <58671211+erikfotta@users.noreply.github.com>
2020-03-02 02:46:52 -0500

    Announcements Rough Final

--- Project7/Project7/AnnouncementsTableViewController.swift
+++ Project7/Project7/AnnouncementsTableViewController.swift
@@ -10,97 +10,105 @@ import UIKit
 import Foundation
 
 class AnnouncementsTableViewController: UITableViewController {
-
-    //Test Variable Declarations
-    let titles: [String] = ["Title 0","Title 1","Title 2","Title 3","Title 4","Title 5"]
-    let senders: [String] = ["Sender 0","Sender 1","Sender 2","Sender 3","Sender 4","Sender 5"]
-    let bodys: [String] = ["""
-    Following this week’s upcoming ASM address from author and environmentalist Bill McKibben, we will host a “special edition” of the Climate Café in the Freeman room of the Library from 12:10-1:25.  An array of sandwiches, salad, and cookies will be made available for attendees.  We hope this will be a great Q&A opportunity for interested students!
-
-    More on Mr. McKibben:
-
-    Bill McKibben is an author and environmentalist who in 2014 was awarded the Right Livelihood Prize, sometimes called the ‘alternative Nobel.’ His 1989 book The End of Nature is regarded as the first book for a general audience about climate change, and has appeared in 24 languages; he’s gone on to write a dozen more books. He is a founder of 350.org
-
-    350.org: A global campaign to confront the climate crisis
-    We are standing up to the fossil fuel industry to stop all new coal, oil and gas projects and build clean energy for all.
-    350.org
-    , the first planet-wide, grassroots climate change movement, which has organized twenty  thousand rallies around the world in every country save North Korea, spearheaded the resistance to the Keystone Pipeline, and launched the fast-growing fossil fuel divestment movement.
-
-    The Schumann Distinguished Scholar in Environmental Studies at Middlebury College and a fellow of the American Academy of Arts and Sciences, he was the 2013 winner of the Gandhi Prize and the Thomas Merton Prize, and holds honorary degrees from 18 colleges and universities. Foreign Policy named him to their inaugural list of the world’s 100 most important global thinkers, and the Boston Globe said he was “probably America’s most important environmentalist.”
-
-    A former staff writer for the New Yorker, he writes frequently for a wide variety of publications around the world, including the New York Review of Books,National Geographic, and Rolling Stone. He lives in the mountains above Lake Champlain with his wife, the writer Sue Halpern, where he spends as much time as possible outdoors . In 2014, biologists honored him by naming a new species of woodland gnat— Megophthalmidia mckibbeni--in his honor.
-
-    Hope to see you there!
-
-    Ms. Guerette, Mr. Curtis & Mr. Barker
-"""
-        ,"Body 1","Body 2","Body 3","Body 4","Body 5"]
-    let groups: [String] = ["Group 0","Group 1","Group 2","Group 3","Group 4","Group 5",]
-    let times: [String] = ["12:00","12:01","12:02","12:03","12:04","12:05",]
     
     // Variable Declarations
     let defaults = UserDefaults.standard
     var data: DataLayer = DataLayer(userID: 1)
-    
     let abbotBlue: UIColor = UIColor(red: 102/255, green: 173/255, blue: 220/255, alpha: 1)
-   
+    var announcementsList: [Announcement] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.backgroundColor = abbotBlue
+        viewDidAppear(true)
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        let announcementsData = data.getAnnouncements()!
-        print(announcementsData)
-        print(type(of: announcementsData))
+        let announcementsData: [Announcement] = data.getAnnouncements()!.announcements
+        announcementsList = announcementsData
+        print(announcementsList[0].title)
+    }
+    
+    private func declare() -> [Announcement] {
+        let announcementsData: [Announcement] = data.getAnnouncements()!.announcements
+        return announcementsData
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if titles.count != 0 {
-//            print(announcements.count)
-            return titles.count
+        if announcementsList.count != 0 {
+            return announcementsList.count
         } else {
             print("Error with fetching announcements")
             return 0
         }
     }
 
+    // Date parsing function to convert the string from the JSON formed struct
+    func dateParse(inputDate: String) -> Date {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.dateFormat = "yyyy-dd-MM'T'HH:mm:ssZ"
+        let date = formatter.date(from: inputDate)!
+        return date
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       
-//        let announcementsData = data.getAnnouncements()!
-//      Go steal from white house app
-        
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "AnnouncementCell", for: indexPath)
         if let announcementCell = cell as? AnnouncementsTableViewCell {
             
-            announcementCell.titleLabel?.text = titles[indexPath.row]
-            announcementCell.senderLabel?.text = senders[indexPath.row]
-            announcementCell.bodyLabel?.text = bodys[indexPath.row]
-            announcementCell.groupLabel?.text = groups[indexPath.row]
-            announcementCell.timeLabel?.text = times[indexPath.row]
+            // Further date parsing for string reformatting
+            let date = dateParse(inputDate: announcementsList[indexPath.row].datetime)
+            let calendar = Calendar.current
+            let day = calendar.component(.day, from: date)
+            let month = calendar.component(.month, from: date)
+            let hour = calendar.component(.hour, from: date)
+            var minute = "\(calendar.component(.minute, from: date))"
+            // Makes minutes under 10 appear in their proper form. ex. '02', rather than '2'
+            if minute.count < 2 {
+                minute = "0\(minute)"
+            } else {}
+            let shownDate = "\(hour):\(minute), \(month)/\(day)"
+            
+            //Cell label population
+            announcementCell.titleLabel?.text = announcementsList[indexPath.row].title
+            announcementCell.senderLabel?.text = "\(announcementsList[indexPath.row].senderfirstname) \(announcementsList[indexPath.row].senderlastname ?? "")"
+            announcementCell.bodyLabel?.text = announcementsList[indexPath.row].body
+            announcementCell.groupLabel?.text = announcementsList[indexPath.row].program
+            announcementCell.timeLabel?.text = shownDate
             return announcementCell
         }
         return cell
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("\(indexPath.row) tapped")
         
+        // Further date parsing for formatting
+        let date = dateParse(inputDate: announcementsList[indexPath.row].datetime)
+        let calendar = Calendar.current
+        let day = calendar.component(.day, from: date)
+        let month = calendar.component(.month, from: date)
+        let hour = calendar.component(.hour, from: date)
+        var minute = "\(calendar.component(.minute, from: date))"
+        if minute.count < 2 {
+            minute = "0\(minute)"
+        } else {}
+        let shownDate = "\(hour):\(minute), \(month)/\(day)"
+        
+        // New VC Instantiation
         let storyboard = UIStoryboard(name: "AnnouncementsStoryboard", bundle: nil)
         let selectedVC = storyboard.instantiateViewController(identifier: "SelectedVC") as! SelectedAnnouncementViewController
-       
-        let slcAnnouncement: [String] = ["\(titles[indexPath.row])","\(senders[indexPath.row])","\(bodys[indexPath.row])","\(groups[indexPath.row])","\(times[indexPath.row])"]
         
+        // String compiled for User Defaults
+        let slcAnnouncement: [String] = ["\(announcementsList[indexPath.row].title)","\(announcementsList[indexPath.row].senderfirstname) \(announcementsList[indexPath.row].senderlastname ?? "")","\(announcementsList[indexPath.row].body)","\(announcementsList[indexPath.row].program)","\(shownDate)"]
         defaults.set(slcAnnouncement, forKey: "slcAnnouncement" )
-        
+       
+        // Instantiated VC push
         navigationController?.pushViewController(selectedVC, animated: true)
     }
     
