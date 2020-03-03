ade40051975787bd155fde02748aa3b82036d698
esly20 <emslibeck@gmail.com>
2020-03-02 21:47:13 -0500

    using data in activity section

--- Project7/Project7/EmmaFolder/ActivityTableViewController.swift
+++ Project7/Project7/EmmaFolder/ActivityTableViewController.swift
@@ -18,11 +18,11 @@ class ActivityTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.backgroundColor = abbotBlue
-        
+
         activityList = data.getActivities()!.activities
         studentActivityList = data.getStudentActivities()!.activities
         tableView.reloadData()
-//        let urlStringActivity = "https://summer-session-api.herokuapp.com/activities"
+        //        let urlStringActivity = "https://summer-session-api.herokuapp.com/activities"
 //        let urlStringStudentActivity = "https://summer-session-api.herokuapp.com/student/1/activities"
 //
 //        if let url = URL(string: urlStringActivity) {
@@ -150,50 +150,4 @@ class ActivityTableViewController: UITableViewController {
                 tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
